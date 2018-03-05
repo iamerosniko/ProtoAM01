e51f1b0dd20c9a191f491b80c82f8709e0c70d46
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-03-05 15:39:47 -0500

    xcreate controllers / services for business workflow

---
+++ BusinessWorkflow/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessWorkflow.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BusinessWorkflow.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        public UsersController()
+        {
+            _api = new ApiAccess("Users1");
+        }
+        private ApiAccess _api;
+        // GET: api/Users
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = _api.GetRequest().Result;
+            if (result == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(result);// new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            return "value";
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody]string value)
+        {
+            return "value";
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ BusinessWorkflow/Utility/ApiAccess.cs
@@ -0,0 +1,130 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessWorkflow.Utility
+{
+    public class ApiAccess
+    {
+        private string _apiURL;
+        private string _apiToken;
+        private HttpClient _client;
+
+        public ApiAccess(string controller)
+        {
+            _apiURL = Startup.Configuration["APISection:ApiURL"];
+            _apiURL = _apiURL + controller;
+
+            _client = new HttpClient();
+
+        }
+
+        public void AssignAuthorization(string token)
+        {
+            _apiToken = token;
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiToken);
+        }
+
+
+        public async Task<string> PostRequest(string body)
+        {
+            var content = new StringContent(body, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var request = await _client.PostAsync(_apiURL, content);
+
+                if (request.IsSuccessStatusCode)
+                {
+                    var result = request.Content.ReadAsStringAsync().Result;
+                    return result;
+                }
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<string> PutRequest(string id, string body)
+        {
+            var content = new StringContent(body, Encoding.UTF8, "application/json");
+            var apiUrl = _apiURL + "/" + id;
+
+            try
+            {
+                var request = await _client.PutAsync(apiUrl, content);
+
+                if (request.IsSuccessStatusCode)
+                {
+                    var result = request.Content.ReadAsStringAsync().Result;
+                    return result;
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> DeleteRequest(string id)
+        {
+            bool isSuccess = false;
+            try
+            {
+                var request = await _client.DeleteAsync(_apiURL + "/" + id);
+
+                isSuccess = request.IsSuccessStatusCode;
+            }
+            catch
+            {
+                return false;
+            }
+
+            return isSuccess;
+        }
+
+        public async Task<string> GetRequest()
+        {
+            try
+            {
+                var request = await _client.GetAsync(_apiURL);
+                if (request.IsSuccessStatusCode)
+                {
+                    var result = request.Content.ReadAsStringAsync().Result;
+                    return result;
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        public async Task<string> GetRequest(string id)
+        {
+            try
+            {
+                var request = await _client.GetAsync(_apiURL + "/" + id);
+                if (request.IsSuccessStatusCode)
+                {
+                    var result = request.Content.ReadAsStringAsync().Result;
+                    return result;
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
