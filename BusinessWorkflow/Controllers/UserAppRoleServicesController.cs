395abbb48580a39ed6bbfc24f93b8f9934ddd0cf
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-03-12 07:35:22 -0400

    created userapproleservices providers

---
+++ BusinessWorkflow/Controllers/UserAppRoleServicesController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessWorkflow.Models;
+using BusinessWorkflow.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessWorkflow.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/UserAppRoleServices")]
+    public class UserAppRoleServicesController : Controller
+    {
+        private UserAppRoleServiceProviders _providers;
+
+        [HttpGet]
+        public async Task<List<AM_UserAppRoleService>> Get()
+        {
+            _providers = new UserAppRoleServiceProviders(HttpContext.Session.GetString("authorizationToken"));
+            var userAppRoleServices = await _providers.get();
+            return userAppRoleServices;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AM_UserAppRoleService> Get(string id)
+        {
+            _providers = new UserAppRoleServiceProviders(HttpContext.Session.GetString("authorizationToken"));
+            var userAppRoleService = await _providers.get(id);
+            return userAppRoleService;
+        }
+
+        [HttpPost]
+        public async Task<AM_UserAppRoleService> Post([FromBody]AM_UserAppRoleService value)
+        {
+            _providers = new UserAppRoleServiceProviders(HttpContext.Session.GetString("authorizationToken"));
+
+            var result = await _providers.Post(value);
+
+            return result;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<AM_UserAppRoleService> Put(string id, [FromBody]AM_UserAppRoleService value)
+        {
+            _providers = new UserAppRoleServiceProviders(HttpContext.Session.GetString("authorizationToken"));
+
+            var result = await _providers.Put(id, value);
+
+            return result;
+        }
+
+        [HttpDelete("{id}")]
+        public async void Delete(string id)
+        {
+            _providers = new UserAppRoleServiceProviders(HttpContext.Session.GetString("authorizationToken"));
+            await _providers.Delete(id);
+        }
+    }
+}
---
+++ BusinessWorkflow/Services/UserAppRoleServiceProviders.cs
@@ -0,0 +1,115 @@
+﻿using BusinessWorkflow.Models;
+using BusinessWorkflow.Utility;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessWorkflow.Services
+{
+    public class UserAppRoleServiceProviders : Controller
+    {
+        private ApiServices _api;
+        private string _authorizationtoken;
+
+        public UserAppRoleServiceProviders(string token)
+        {
+            _authorizationtoken = token;
+        }
+
+        #region API
+
+        #region GET
+        public async Task<List<AM_UserAppRoleService>> get()
+        {
+            List<AM_UserAppRoleService> userAppRoleServices = new List<AM_UserAppRoleService>();
+            bindApiServices();
+
+            var result = await _api.Get();
+            try
+            {
+                userAppRoleServices = result == null ? null : JsonConvert.DeserializeObject<List<AM_UserAppRoleService>>(result);
+
+            }
+            catch
+            {
+                return userAppRoleServices;
+            }
+
+
+            return userAppRoleServices;
+        }
+
+        public async Task<AM_UserAppRoleService> get(string id)
+        {
+            AM_UserAppRoleService userAppRoleService = new AM_UserAppRoleService();
+            bindApiServices();
+
+            var result = await _api.Get(id);
+            try
+            {
+                userAppRoleService = result == null ? null : JsonConvert.DeserializeObject<AM_UserAppRoleService>(result);
+
+            }
+            catch
+            {
+                return userAppRoleService;
+            }
+
+
+            return userAppRoleService;
+        }
+        #endregion
+
+        #region POST
+        public async Task<AM_UserAppRoleService> Post(AM_UserAppRoleService userAppRoleService)
+        {
+            bindApiServices();
+
+            string body = JsonConvert.SerializeObject(userAppRoleService);
+
+            var result = await _api.Post(body);
+
+            userAppRoleService = result != null ? JsonConvert.DeserializeObject<AM_UserAppRoleService>(result) : null;
+
+            return userAppRoleService;
+        }
+
+        #endregion
+
+        #region PUT
+        public async Task<AM_UserAppRoleService> Put(string id, AM_UserAppRoleService userAppRoleService)
+        {
+            bindApiServices();
+            string body = JsonConvert.SerializeObject(userAppRoleService);
+
+            var result = await _api.Put(id, body);
+
+            userAppRoleService = result != null ? JsonConvert.DeserializeObject<AM_UserAppRoleService>(result) : null;
+
+            return userAppRoleService;
+        }
+
+        #endregion
+
+        #region DELETE
+        public async Task<bool> Delete(string id)
+        {
+            bindApiServices();
+
+            var result = await _api.Delete(id);
+            return result;
+        }
+
+        #endregion
+
+        #endregion
+
+
+
+        private void bindApiServices()
+        {
+            _api = new ApiServices("UserAppRoleServices", _authorizationtoken);
+        }
+    }
+}
