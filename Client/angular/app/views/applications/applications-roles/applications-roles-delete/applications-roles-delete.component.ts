c3d478905f84d41785ee860b3dbec986e08ea366
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-04-23 16:56:54 -0400

    up

--- Client/angular/app/views/applications/applications-roles/applications-roles-delete/applications-roles-delete.component.ts
+++ Client/angular/app/views/applications/applications-roles/applications-roles-delete/applications-roles-delete.component.ts
@@ -1,4 +1,8 @@
+import { Router,ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Roles } from '../../../../entities/btam-entities';
+import { RolesService } from '../../../../services/client.services';
+
 
 @Component({
   selector: 'app-applications-roles-delete',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./applications-roles-delete.component.css']
 })
 export class ApplicationsRolesDeleteComponent implements OnInit {
+  role:Roles={};
 
-  constructor() { }
+  constructor(private router:Router,private activatedroute: ActivatedRoute,
+    private roleSvc :RolesService) { }
 
   ngOnInit() {
   }
 
+  async delete() {
+    // var role:Roles =await this.role.deleteApplication(this.app.AppID.toString())
+    // app.AppID==this.app.AppID 
+    // ?(
+    //   alert("Successfully saved!"),
+    //   await this.goBack()
+    // ):null;
+  }
 }
